93f72acb7c07dc29791136171e3612b7f12a7a35
noname13a <83711843+noname13a@users.noreply.github.com>
2023-06-05 18:21:37 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,22 @@
+# echo-server.py
+
+import socket
+
+HOST = "192.168.1.100"  #Server address
+PORT = 9001  #Server port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        conn.sendall(bytes("¿Estas de broma no? Ya te la mande otra vez la semana pasada\n", "utf-8"))
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        conn.sendall(bytes("Lo que hay que ver, encima por un canal no seguro... Ahí la llevas: cobra{st41king_thr0ugh_th3_w1r3}\n", "utf-8"))
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        conn.sendall(bytes("Si... Pero como alguien pille esto, te vas a la calle.\n", "utf-8"))
+    s.close()
